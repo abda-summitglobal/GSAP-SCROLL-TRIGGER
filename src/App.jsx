4bbace76487e0300b7646aa614271a7f3493460b
l13nnn <irenedjaya288@gmail.com>
2025-03-05 11:27:20 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+function ImageBox({ imgSrc }) {
+  const boxRef = useRef(null);
+
+  
+
+  return (
+    <div>
+      <div className='box-container'>
+        <div ref={boxRef} className="box" style={{width: '150px', height: '150px', marginBlock: '50px '}}>
+          <img src={imgSrc} style={{width: '100%', height: '100%'}} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  const imageSources = [
+    "https://th.bing.com/th/id/R.388a295524209dce7d4bcc4d44e1ed97?rik=zR1t0wGUX%2bn6bg&riu=http%3a%2f%2fwww.missionmission.org%2fwp-content%2fuploads%2f2014%2f02%2fcupcake.jpg&ehk=k%2f0X6uOfQLTtf7S4tpZ6fXol6aTahwOpXXn2eCHnlyg%3d&risl=&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.ce9a5abd998e51fbe87d1308d8c77cdf?rik=T16rYnFnQv2kJA&riu=http%3a%2f%2fwww.yourcupofcake.com%2fwp-content%2fuploads%2f2012%2f03%2fPink-Confetti.jpg&ehk=4GleROE7KwEUeyhKam%2btizTCOO1ZN4AyLzTVfn5G048%3d&risl=&pid=ImgRaw&r=0",
+    "https://www.recipetineats.com/wp-content/uploads/2020/09/Vanilla-Cupcakes-with-Vanilla-Swiss-Meringue-SQ.jpg",
+    "https://th.bing.com/th/id/OIP.Jx1EP214SsFaNM5tNmgyjAHaHa?rs=1&pid=ImgDetMain",
+    "https://th.bing.com/th/id/OIP.2odbmslqbAwDbfyN77mukgHaHa?rs=1&pid=ImgDetMain",
+  ];
+
+  useEffect(() => {
+    gsap.to(
+      '.box',
+      {
+        scrollTrigger: {
+          trigger: '.container2',
+          start: 'top top',
+          end: '100%',
+          scrub: true,
+          markers: true,
+          pin: true,
+          toggleActions: 'play none none reverse',
+        },
+      }
+    )
+  }, []);
+
+  return (
+    <div className='container' style={{height: 'unsets', width: '100vw'}}>
+      <div className='container1' style={{height: '50vh'}}></div>
+      <div className='container2' style={{height: '100vh'}}>
+        <div className="box" style={{width: '150px', height: '150px', marginBlock: '50px '}}>
+          <img src={'https://th.bing.com/th/id/OIP.2odbmslqbAwDbfyN77mukgHaHa?rs=1&pid=ImgDetMain'} style={{width: '100%', height: '100%'}} />
+        </div>
+        {/* {imageSources.map((src, index) => (
+          <ImageBox imgSrc={src} index={index} key={index} />
+        ))} */}
+      </div>
+    </div>
+  )
+}
+
+export default App;
